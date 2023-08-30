ff238779b97682c0dd367a6c5422a0e2c0e0f4cd
Anderson Souza <anderson_razor_88@hotmail.com>
2023-08-30 20:23:41 -0300

    criando a controller do sistemas e começando a organizar as rotas

---
+++ app/Http/Controllers/SistemaController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SistemaController extends Controller
+{
+   /**
+    * Cria um novo Dentista
+    *
+    * @return void
+    */
+    public function create()
+    {
+        echo "aqui é a criar";
+    }
+
+    /**
+     * Mostra a lista de dentistas
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo "mostra a lista de dentistas";
+    }
+
+    /**
+     * Mostrar o dentista por id
+     */
+
+    public function show(int $id){
+       $dentistas = [
+        1=> [
+            'nome'=> 'Batman',
+            'especialidade'=> 'arrancar dente na porrada'
+        ],
+        2=> [
+            'nome'=> 'Robin',
+            'especialidade'=> 'arrancar dente no tapa'
+        ],
+        3=> [
+            'nome'=> 'Superman',
+            'especialidade'=> 'arrancar dente com sopro'
+        ],
+
+       ];
+
+       echo $dentistas[$id]['nome'];
+       echo "<br>";
+       echo $dentistas[$id]['especialidade'];
+    }
+
+    /**
+     * Salva no Banco 
+     *
+     * @return void
+     */
+    public function store()
+    {
+
+    }
+
+    /**
+     * edita o dentista
+     */
+
+    public function edit()
+    {
+        echo "teste editar";
+    }
+
+    /**
+     * salva no banco a edição
+     *
+     * @return void
+     */
+    public function update()
+    {
+
+    }
+    /**
+     * deleta o dentista
+     *
+     * @return void
+     */
+    public function destroy()
+    {
+        echo "teste excluir";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\SistemaController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[SistemaController::class,'create']);
+Route::get('/lista',[SistemaController::class,'index']);
+Route::get('/listar/{id}',[SistemaController::class,'show']);
+Route::get('/editar',[SistemaController::class, 'edit']);
+
